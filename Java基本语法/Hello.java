38478fdf58c1fc433ecf6722cede9d08c74d4144
coderabbit6 <2416841692@qq.com>
2018-08-13 10:21:16 +0800

    hello

---
+++ Java基本语法/Hello.java
@@ -0,0 +1,13 @@
+package hello;
+
+import java.util.Scanner;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello,java");
+		Scanner in = new Scanner(System.in);
+		System.out.println(in.next());
+	}
+}
